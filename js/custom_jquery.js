38331d30fd5aff84cf35544f6518cad59525d0d8
Heinrich Hohbach <heinrich.hohbach@gmail.com>
2018-03-08 19:07:54 +0000

    JQuery Validation Reload
    Reloading page after valid information entered.

--- js/custom_jquery.js
+++ js/custom_jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-			<!-- Real-time Validation -->
-				<!--Name can not be blank-->
+//			<!-- Real-time Validation -->
+//				<!--Name can not be blank-->
 				$('#inputName').on('input', function() {
 					var input=$(this);
 					var is_name=input.val();
@@ -8,7 +8,7 @@ $(document).ready(function() {
 					else{input.removeClass("valid").addClass("invalid");}
 				});
 				
-				<!--Email must be an email -->
+//				<!--Email must be an email -->
 				$('#inputEmail').on('input', function() {
 					var input=$(this);
 					var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -17,7 +17,7 @@ $(document).ready(function() {
 					else{input.removeClass("valid").addClass("invalid");}
 				});
 				
-				<!--Message can not be blank -->
+//				<!--Message can not be blank -->
 				$('#inputMessage').keyup(function(event) {
 					var input=$(this);
 					var message=$(this).val();
@@ -26,8 +26,9 @@ $(document).ready(function() {
 					else{input.removeClass("valid").addClass("invalid");}	
 				});
 		
-			<!-- After Form Submitted Validation-->
+//			<!-- After Form Submitted Validation-->
 			$("#inputSubmit button").click(function(event){
+                event.preventDefault();
 				var form_data=$("#contactform").serializeArray();
 				var error_free=true;
 				for (var input in form_data){
@@ -37,11 +38,9 @@ $(document).ready(function() {
 					if (!valid){error_element.removeClass("error").addClass("error_show"); error_free=false;}
 					else{error_element.removeClass("error_show").addClass("error");}
 				}
-				if (!error_free){
-					event.preventDefault(); 
-				}
-				else{
+				if (error_free){
 					alert('No errors: Form will be submitted');
+                    location.reload();
 				}
 			});
 			
