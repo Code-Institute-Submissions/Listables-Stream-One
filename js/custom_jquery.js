f4485d10fe57f9d348b546ba465d91b4460a0e6c
Heinrich Hohbach <heinrich.hohbach@gmail.com>
2018-04-08 21:30:44 +0100

    Improvement of JQuery
    Improvement of JQuery files for form validation.

--- js/custom_jquery.js
+++ js/custom_jquery.js
@@ -1,59 +1,49 @@
 $(document).ready(function () {
     //			<!-- Real-time Validation -->
-    //				<!--Name can not be blank-->
-    $('#inputName').on('input', function () {
-        var input = $(this);
-        var is_name = input.val();
-        if (is_name) {
-            input.removeClass("invalid").addClass("valid");
+    //				<!--Name and Message can not be blank-->
+    $('#inputName, #inputMessage').on('input', function () {
+        if (validateString($(this).val())) {
+            $(this).removeClass('invalid').addClass('valid');
         } else {
-            input.removeClass("valid").addClass("invalid");
+            $(this).removeClass('valid').addClass('invalid');
         }
     });
 
     //				<!--Email must be an email -->
     $('#inputEmail').on('input', function () {
-        var input = $(this);
-        var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        var is_email = re.test(input.val());
-        if (is_email) {
-            input.removeClass("invalid").addClass("valid");
+        if (validateEmail($(this).val())) {
+            $(this).removeClass('invalid').addClass('valid');
         } else {
-            input.removeClass("valid").addClass("invalid");
-        }
-    });
-
-    //				<!--Message can not be blank -->
-    $('#inputMessage').keyup(function (event) {
-        var input = $(this);
-        var message = $(this).val();
-        console.log(message);
-        if (message) {
-            input.removeClass("invalid").addClass("valid");
-        } else {
-            input.removeClass("valid").addClass("invalid");
+            $(this).removeClass('valid').addClass('invalid');
+            hasErrors = true;
         }
     });
 
     //			<!-- After Form Submitted Validation-->
-    $("#inputSubmit button").click(function (event) {
+    $("#contactform").submit(function (event) {
         event.preventDefault();
-        var form_data = $("#contactform").serializeArray();
-        var error_free = true;
+        var form_data = $(this).serializeArray();
+        var hasErrors = false;
         for (var input in form_data) {
-            var element = $("#input" + form_data[input]['name']);
-            var valid = element.hasClass("valid");
-            var error_element = $("span", element.parent());
+            if (!form_data.hasOwnProperty(input)) {
+                continue;
+            }
+            
+            var element = $('#input' + form_data[input]['name']);
+            var valid = element.hasClass('valid');
+            var error_element = $('span', element.parent());
             if (!valid) {
-                error_element.removeClass("error").addClass("error_show");
-                error_free = false;
+                error_element.removeClass('error').addClass('error_show');
+                hasErrors = true;
             } else {
-                error_element.removeClass("error_show").addClass("error");
+                error_element.removeClass('error_show').addClass('error');
             }
         }
-        if (error_free) {
-            alert('Thanks for contacting us! We will get back to you as soon as we can!');
+        
+        if (!hasErrors) {
+            alert('Thanks for contacting us! We will get back to you as soon as possible!');
             location.reload();
+            return true;
         }
     });
 
@@ -82,3 +72,14 @@ $(document).ready(function () {
     });
 
 });
+
+// Validate that a string isn't blank
+var validateString = function (value) {
+    return value.length > 0;
+};
+
+// Validate the email address
+var validateEmail = function (email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+    return re.test(email);
+}
