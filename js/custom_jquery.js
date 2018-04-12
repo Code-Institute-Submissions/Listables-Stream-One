9f6cc21c8f645949fa51f438866b94aef8b82001
Heinrich Hohbach <heinrich.hohbach@gmail.com>
2018-04-12 21:08:08 +0100

    Added JS/CSS comments
    added JS/CSS comments to make the code easier to understand.

--- js/custom_jquery.js
+++ js/custom_jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    //			<!-- Real-time Validation -->
-    //				<!--Name and Message can not be blank-->
+    // The Name and Message Fields have to be entered to make sure that the website owner can contact them back.
+    // This code makes sure that these fields are in fact filled in.
     $('#inputName, #inputMessage').on('input', function () {
         if (validateString($(this).val())) {
             $(this).removeClass('invalid').addClass('valid');
@@ -9,7 +9,8 @@ $(document).ready(function () {
         }
     });
 
-    //				<!--Email must be an email -->
+    // The email field needs to be entered correctly to ensure that the website owner can contact the person back.
+    // This code makes sure that the email field has a valid email entered.
     $('#inputEmail').on('input', function () {
         if (validateEmail($(this).val())) {
             $(this).removeClass('invalid').addClass('valid');
@@ -19,7 +20,8 @@ $(document).ready(function () {
         }
     });
 
-    //			<!-- After Form Submitted Validation-->
+    // The fields need to be validated after submission to ensure that the information is correct and passed on.
+    //This code performs the validation and then shows a notification if the user input was valid.
     $("#contactform").submit(function (event) {
         event.preventDefault();
         var form_data = $(this).serializeArray();
@@ -47,6 +49,8 @@ $(document).ready(function () {
         }
     });
 
+    // The accordion used in the Service Page needs this code to function.
+    // Without this code the accordion wouldn't open and close.
     $('.accordion-toggle').on('click', function (event) {
         event.preventDefault();
         // create accordion variables
--- js/jQuery_Tests.js
+++ js/jQuery_Tests.js
@@ -1,15 +1,19 @@
+// This Test passes a string into the 'validateString' method to test if the method works.
 QUnit.test('validateString_Success', function (assert) {
     assert.ok(validateString('Teststring'), 'Passed!');
 });
 
+// This test passes an empty string into the 'validateString' method to make sure that the method works with an empty string.
 QUnit.test('validateString_Fail', function (assert) {
     assert.notOk(validateString(''), 'Passed!');
 });
 
+// This test passes a valid email into the 'validateEmail' method to test if the method actually validates emails
 QUnit.test('validateEmail_Success', function (assert) {
     assert.ok(validateEmail('test@test.com'), 'Passed!');
 });
 
+// This test passes a wrong email into the method and see if it fails. This makes sure that the method works both ways.
 QUnit.test('validateEmail_Fail', function (assert) {
     assert.notOk(validateEmail('test@test.123'), 'Passed!');
 });
